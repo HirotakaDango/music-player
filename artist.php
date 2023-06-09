66266e65909ea100bf891285385e648210365e64
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-06-10 01:17:26 +0700

    Add files via upload

--- artist.php
+++ artist.php
@@ -54,7 +54,7 @@ $filteredSongs = array_filter($songList, function($song) use ($artistName) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Music Player</title>
+    <title><?php echo $artistName; ?></title>
   </head>
 
   <body>
